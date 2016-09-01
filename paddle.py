2e35352f52079c43d24c0d0cfe99b70a94eadc7e
Kenny Vu <kemyvu@gmail.com>
2016-09-01 20:06:45 +0100

    The paddles now stop when they reach the edge of the screen

--- paddle.py
+++ paddle.py
@@ -6,12 +6,22 @@ class Paddle:
         self.y = 0
         self.canvas.bind_all(upKey, self.go_up)
         self.canvas.bind_all(downKey, self.go_down)
+        self.canvas_height = self.canvas.winfo_height()
 
     def draw(self):
         self.canvas.move(self.id, 0, self.y)
+        self.pos = self.canvas.coords(self.id)
+        if self.pos[1] <=0 or self.pos[3] >= self.canvas_height:
+            self.y = 0
 
     def go_up(self, evt):
-        self.y = -2
+        if self.pos[1] <=0:
+            self.y = 0
+        else:
+            self.y = -2
 
     def go_down(self, evt):
-        self.y = 2
+        if self.pos[3] >= self.canvas_height:
+            self.y = 0
+        else:
+            self.y = 2
