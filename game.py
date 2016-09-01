aa8e88e4f9a5de3891962d1c8d1a034dbf026de3
Kenny Vu <kemyvu@gmail.com>
2016-09-01 16:42:31 +0100

    Add ball and the two paddles

--- game.py
+++ game.py
@@ -1,4 +1,6 @@
 from tkinter import *
+from ball import *
+from paddle import *
 from configure import Config
 import time
 
@@ -9,7 +11,17 @@ def main():
     canvas.pack()
     tk.update()
 
+    ball = Ball(canvas,"red")
+    paddle1 = Paddle(canvas, 0, 0, "blue")
+    paddle2 = Paddle(canvas, 490, 0, "green")
+
+    #config = Config()
+    mainloop(tk,ball)
+
+
+def mainloop(tk, ball):
     while 1:
+        ball.draw()
         tk.update_idletasks()
         tk.update()
         time.sleep(0.01)
