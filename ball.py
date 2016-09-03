469c3744fb384074bb192a7183a9256e2fd60271
Kenny Vu <kemyvu@gmail.com>
2016-09-03 14:07:41 +0100

    The ball now moves depending on where it hit the paddles

--- ball.py
+++ ball.py
@@ -4,23 +4,36 @@ class Ball:
         self.paddle1 = paddle1
         self.paddle2 = paddle2
         self.x = -3
+        self.y = 0
         self.id = canvas.create_oval(10, 10, 25, 25, fill=color)
         self.canvas.move(self.id, 250, 250)
 
     def draw(self):
-        self.canvas.move(self.id, self.x, 0)
+        self.canvas.move(self.id, self.x, self.y)
         pos = self.canvas.coords(self.id)
-        if self.hit_paddle2(pos) == True:
-            self.x = -3
-        if self.hit_paddle1(pos) == True:
+        if self.hit_paddle(self.paddle1, pos) == True:
             self.x = 3
+            self.hit_location(self.paddle1, pos)
+            #self.y = -1
+        if self.hit_paddle(self.paddle2, pos) == True:
+            self.x = -3
+            self.hit_location(self.paddle2, pos)
+            #self.y = 1
 
-    def hit_paddle1(self, pos):
-        paddle_pos = self.canvas.coords(self.paddle1.id)
+    def hit_paddle(self, paddle, pos):
+        paddle_pos = self.canvas.coords(paddle.id)
         if pos[2] >= paddle_pos[2] and pos[0] <= paddle_pos[2]:
             if pos[3] >= paddle_pos[1] and pos[3] <= paddle_pos[3]:
                 return True
         return False
+
+    def hit_location(self, paddle, pos):
+        paddle_pos = self.canvas.coords(paddle.id)
+        centre = (paddle_pos[1] + paddle_pos[3]) / 2
+        if pos[3] > centre:
+            self.y = 1
+        elif pos[3] < centre:
+            self.y = -1
 
     def hit_paddle2(self, pos):
         paddle_pos = self.canvas.coords(self.paddle2.id)
@@ -28,3 +41,4 @@ class Ball:
             if pos[3] >= paddle_pos[1] and pos[3] <= paddle_pos[3]:
                 return True
         return False
+
