3c4a3370fb561d0c9a59e520792a9afd125139f8
Kenny Vu <kemyvu@gmail.com>
2016-09-04 16:18:27 +0100

    Created class for displaying text

---
+++ text_display.py
@@ -0,0 +1,4 @@
+class TextDisplay:
+    def __init__(self, canvas, startX, startY, text):
+        self.canvas = canvas
+        self.id = self.canvas.create_text(startX, startY, text=text, font=('Times', 15))
