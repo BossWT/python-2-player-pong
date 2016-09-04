03856c6e8127da04f8ac088de22a8fc63fc1b724
Kenny Vu <kemyvu@gmail.com>
2016-09-04 19:09:05 +0100

    Added comments

--- ball.py
+++ ball.py
@@ -8,10 +8,10 @@ class Ball:
         self.P1Goal = P1Goal
         self.P2Goal = P2Goal
         self.P1Score = P1Score
-        self.score1 = 0
+        self.score1 = 0 #Stores player 1's score
         self.P2Score = P2Score
-        self.score2 = 0
-        self.randomize_movement()
+        self.score2 = 0 #Stores player 2's score
+        self.randomize_movement() #The balls starting position is random
         self.id = canvas.create_oval(10, 10, 25, 25, fill=color)
         self.canvas.move(self.id, 250, 250)
         self.canvas_height = self.canvas.winfo_height();
@@ -41,17 +41,15 @@ class Ball:
     def hit_location(self, paddle, pos):
         paddle_pos = self.canvas.coords(paddle.id)
         centre = (paddle_pos[1] + paddle_pos[3]) / 2
-        if pos[3] > centre:
+        if pos[3] == centre:
+            #if the ball hits the centre of the paddle
+            self.y = 0
+        elif pos[3] > centre:
+            #if the ball hits the top half of the paddle
             self.y = 1
         elif pos[3] < centre:
+            #if the ball hits the bottom half of the paddle
             self.y = -1
-
-    def hit_paddle2(self, pos):
-        paddle_pos = self.canvas.coords(self.paddle2.id)
-        if pos[2] >= paddle_pos[2] and pos[0] <= paddle_pos[2]:
-            if pos[3] >= paddle_pos[1] and pos[3] <= paddle_pos[3]:
-                return True
-        return False
 
     def goal(self, pos):
         P1Goal_pos = self.canvas.coords(self.P1Goal.id)
@@ -59,12 +57,15 @@ class Ball:
         if pos[2] <= P1Goal_pos[2]:
             #Player 2 scores a goal
 
+            #Stop the ball
             self.x = 0
             self.y = 0
 
+            #Get the positive difference from the current position's coordinates to the centre
             xToCen = abs(250-pos[0])
             yToCen = abs(250-pos[1])
 
+            #Move the ball to the centre
             if pos[1] > 250:
                 #print("www " + str(pos[1]))
                 self.canvas.move(self.id, xToCen, -yToCen)
@@ -72,19 +73,26 @@ class Ball:
                 #print("wwwwwwww " + str(pos[1]))
                 self.canvas.move(self.id, xToCen, yToCen)
 
+            #Increment score
             self.score2 += 1
             print("p2 scores")
             self.canvas.itemconfig(self.P2Score.id, text=str(self.score2))
+
+            #Get the ball's new speed
             self.randomize_movement()
 
         elif pos[0] >= P2Goal_pos[0]:
             #Player 1 scores a goal
+
+            #Stop the ball
             self.x = 0
             self.y = 0
 
+            #Get the positive difference from the current position's coordinates to the centre
             xToCen = abs(250-pos[0])
             yToCen = abs(250-pos[1])
 
+            #Move the ball to the centre
             if pos[1] > 250:
                 #print("sss " + str(pos[1]))
                 self.canvas.move(self.id, -xToCen, -yToCen)
@@ -92,9 +100,12 @@ class Ball:
                 #print("ssssss " + str(pos[1]))
                 self.canvas.move(self.id, -xToCen, yToCen)
 
+            #Increment score
             self.score1 += 1
             print("p1 scores")
             self.canvas.itemconfig(self.P1Score.id, text=str(self.score1))
+
+            #Get the ball's new speed
             self.randomize_movement()
 
     def randomize_movement(self):
