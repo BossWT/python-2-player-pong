908472d896491ff063a57931563f3ff28f69862b
Kenny Vu <kemyvu@gmail.com>
2016-09-04 16:18:42 +0100

    Added half-way line and text to display each player's score

--- game.py
+++ game.py
@@ -2,6 +2,7 @@ from tkinter import *
 from ball import *
 from paddle import *
 from score_zone import *
+from text_display import *
 from configure import Config
 import time
 
@@ -9,15 +10,17 @@ def main():
     tk = Tk()
     tk.title = "2 Player Pong"
     canvas = Canvas(tk, height=500, width = 500)
+    canvas.create_line(250, 0, 250, 500)
     canvas.pack()
     tk.update()
 
-    paddle1 = Paddle(canvas, "blue", 20, 200,"<KeyPress-w>", "<KeyPress-s>", "<KeyPress-a>")
+    paddle1 = Paddle(canvas, "blue",20, 200,"<KeyPress-w>", "<KeyPress-s>", "<KeyPress-a>")
     paddle2 = Paddle(canvas, "green", 475, 200, "<KeyPress-Up>", "<KeyPress-Down>", "<KeyPress-Right>")
-    ball = Ball(canvas, "red", paddle1, paddle2)
-    scorezone1 = ScoreZone(canvas, "yellow", 0, 0)
-    scorezone2 = ScoreZone(canvas, "yellow", 485, 0)
-
+    P1Goal = ScoreZone(canvas, "yellow", 0, 0)
+    P2Goal = ScoreZone(canvas, "yellow", 485, 0)
+    P1Score = TextDisplay(canvas, 200, 20, "0")
+    P2Score = TextDisplay(canvas, 300, 20, "0")
+    ball = Ball(canvas, "red", paddle1, paddle2, P1Goal, P2Goal)
     #config = Config()
     mainloop(tk,ball,paddle1,paddle2)
 
