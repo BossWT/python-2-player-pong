83977739c2e91d71a2ad68e0bf15898c90a61556
bosswt <boss.wt13@gmail.com>
2020-03-20 18:03:30 +0700

    Change font family and font size

--- text_display.py
+++ text_display.py
@@ -1,4 +1,4 @@
 class TextDisplay:
     def __init__(self, canvas, startX, startY, text):
         self.canvas = canvas
-        self.id = self.canvas.create_text(startX, startY, text=text, font=('Times', 15))
+        self.id = self.canvas.create_text(startX, startY, text=text, font=('terminal', 35, 'bold'))
